67a3ed5fb5f4f8381f5fa442bfa3a62a3f41f0e6
Haniya Ariyil <ariyihaniya@gmail.com>
2026-05-13 21:14:27 +0530

    deployment

--- backend/index.js
+++ backend/index.js
@@ -1,21 +1,21 @@
-import express from "express";
 import dotenv from "dotenv";
+dotenv.config();
+
+import express from "express";
+
 import { connectDB } from "./config/db.js";
 import { ENV_VARS } from "./config/envVars.js";
 import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
 
-dotenv.config();
-
 const app = express();
-const PORT = ENV_VARS.PORT;
+
+connectDB();
 
 app.use(express.json());
+
 app.use(cors());
 
 app.use("/api/product", productRoutes);
 
-app.listen(PORT, () => {
-    console.log("server run at port", +PORT);
-    connectDB();
-});
+export default app;
